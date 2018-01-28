b9b6ea40667baf2c3c6186926941dfeaa150f8c1
Jennifer Jiang2 <jiangbingqianj@gmail.com>
2018-01-28 00:05:39 -0500

    add link property, use JQLite

--- examples/Lecture31/test.js
+++ examples/Lecture31/test.js
@@ -63,14 +63,33 @@
 			bindToController: true,
 			controllerAs: "myList",
 			templateUrl: "myList.html",
+			link: MyListLink,
 		};
 		return ddo;
+	}
+
+	function MyListLink(scope, element, attrs, controller) {
+		console.log("scope is :" , scope);
+		// console.log("element is :", element);
+		// console.log("attrs is : ", attrs);
+		// console.log("controller is :", controller);
+		console.log("this is :", this);
+		// console.log(myList.cookieDetector);
+		//Use Angular built in JQLite
+		scope.$watch(() => scope.myList.cookieDetector(), (newValue) => {
+			// console.log(myList.cookieDetector);
+			var findDiv = element.find('div');
+			if (newValue) {
+				findDiv.css('display', 'block');
+			} else {
+				findDiv.css('display', 'none');
+			}
+		})
 	}
 
 	function MyListController() {
 		var myList = this;
 		myList.cookieDetector = function() {
-			console.log("reach here!");
 			for (let item of myList.items) {
 				if (item.name.toLowerCase().indexOf("cookies") === -1) {
 					return false;
